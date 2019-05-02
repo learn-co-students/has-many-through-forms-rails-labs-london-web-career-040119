7775bd78462a1c42ff2f2c8a4ffd4046e55985bb
Ivan Szebenszki <szib77@gmail.com>
2019-05-02 09:39:53 +0100

    Include blank to select (+10 squashed commits) Squashed commits: [0f65500] Unique username [3cac8c2] Add new comment [704955e] New post [25b1bf5] Migration, seed [7c9f36c] Add categoies to posts/show [b2ec15f] Refactor: posts partial [5dacaaf] categories/show [f5afcd1] users/show [14c87aa] posts/show [0ee9238] Add comment partial

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |attributes|
+      category = Category.find_or_create_by(attributes)
+      categories << category
+    end
+  end
 end
